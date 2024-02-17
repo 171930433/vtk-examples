a7a5004f86684b86694fc2b427679554a69eb142
Andrew Maclean <andrew.amaclean@gmail.com>
2024-02-17 12:25:02 +1100

    Removing redundancies in ParametricKuenDemo.py and fixing range settings

--- src/PythonicAPI/GeometricObjects/ParametricKuenDemo.py
+++ src/PythonicAPI/GeometricObjects/ParametricKuenDemo.py
@@ -31,7 +31,7 @@ def main():
     colors.SetColor('BkgColor', [26, 51, 102, 255])
 
     # Create a parametric function source, renderer, mapper, and actor
-    surface = vtkParametricKuen(minimum_u=-4.5, maximum_u=4.5, minimum_v=0.05, maximum_v=vtkMath.Pi() - .05)
+    surface = vtkParametricKuen(minimum_u=-4.5, maximum_u=4.5, minimum_v=0.05, maximum_v=vtkMath.Pi() - 0.05)
     source = vtkParametricFunctionSource(parametric_function=surface)
     mapper = vtkPolyDataMapper()
     source >> mapper
@@ -55,21 +55,24 @@ def main():
     # Setup a slider widget for each varying parameter.
     slider_dimensions = {'tube_width': 0.008, 'slider_length': 0.008, 'title_height': 0.02, 'label_height': 0.02}
     slider_range_name = {'minimum_value': -4.5, 'maximum_value': 4.5, 'value': -4.5, 'title_text': 'U min'}
-    slider_position = {'point1': (0.1, 0.1), 'point2': (.9, .1)}
+    slider_position = {'point1': (0.1, 0.1), 'point2': (0.9, 0.1)}
     slider_widget_minimum_u = make_slider_widget(slider_dimensions, slider_range_name, slider_position, interactor)
     slider_widget_minimum_u.AddObserver('InteractionEvent', SliderCallbackMinimumU(surface))
 
-    slider_range_name = {'minimum_value': -4.5, 'maximum_value': 4.5, 'value': 4.5, 'title_text': 'U max'}
+    slider_range_name['value'] = 4.5
+    slider_range_name['title_text'] = 'U max'
     slider_position = {'point1': (0.1, 0.9), 'point2': (0.9, 0.9)}
     slider_widget_maximum_u = make_slider_widget(slider_dimensions, slider_range_name, slider_position, interactor)
     slider_widget_maximum_u.AddObserver('InteractionEvent', SliderCallbackMaximumU(surface))
 
-    slider_range_name = {'minimum_value': 0.05, 'maximum_value': vtkMath.Pi(), 'value': 0.0, 'title_text': 'V min'}
+    slider_range_name = {'minimum_value': 0.05, 'maximum_value': vtkMath.Pi() - 0.05, 'value': 0.0,
+                         'title_text': 'V min'}
     slider_position = {'point1': (0.1, 0.1), 'point2': (0.1, 0.9)}
     slider_widget_minimum_v = make_slider_widget(slider_dimensions, slider_range_name, slider_position, interactor)
     slider_widget_minimum_v.AddObserver(vtkCommand.InteractionEvent, SliderCallbackMinimumV(surface))
 
-    slider_range_name = {'minimum_value': -0.05, 'maximum_value': vtkMath.Pi() - 0.5, 'value': vtkMath.Pi(), 'title_text': 'V max'}
+    slider_range_name['value'] = vtkMath.Pi() - 0.05
+    slider_range_name['title_text'] = 'V max'
     slider_position = {'point1': (0.9, 0.1), 'point2': (0.9, 0.9)}
     slider_widget_maximum_v = make_slider_widget(slider_dimensions, slider_range_name, slider_position, interactor)
     slider_widget_maximum_v.AddObserver(vtkCommand.InteractionEvent, SliderCallbackMaximumV(surface))
@@ -126,9 +129,9 @@ class SliderCallbackMinimumU():
     def __call__(self, caller, ev):
         sliderWidget = caller
         value = sliderWidget.representation.value
-        if value > 0.9 * self.kuen.GetMaximumU():
-            value = 0.99 * self.kuen.GetMaximumU()
-            sliderWidget.GetRepresentation().SetValue(value)
+        if value > 0.99 * self.kuen.maximum_u:
+            value = self.kuen.maximum_u
+            sliderWidget.GetRepresentation().value = value
         self.kuen.minimum_u = value
 
 
@@ -139,9 +142,9 @@ class SliderCallbackMaximumU():
     def __call__(self, caller, ev):
         sliderWidget = caller
         value = sliderWidget.representation.value
-        if value < self.kuen.GetMinimumU() + .01:
-            value = self.kuen.GetMinimumU() + .01
-            sliderWidget.GetRepresentation().SetValue(value)
+        if value < self.kuen.minimum_u + .01:
+            value = self.kuen.minimum_u
+            sliderWidget.GetRepresentation().value = value
         self.kuen.maximum_u = value
 
 
@@ -152,9 +155,9 @@ class SliderCallbackMinimumV():
     def __call__(self, caller, ev):
         sliderWidget = caller
         value = sliderWidget.representation.value
-        if value > 0.9 * self.kuen.GetMaximumV():
-            value = 0.99 * self.kuen.GetMaximumV()
-            sliderWidget.GetRepresentation().SetValue(value)
+        if value > 0.99 * self.kuen.maximum_v:
+            value = self.kuen.maximum_v
+            sliderWidget.GetRepresentation().value = value
         self.kuen.minimum_v = value
 
 
@@ -165,9 +168,9 @@ class SliderCallbackMaximumV():
     def __call__(self, caller, ev):
         slider_widget = caller
         value = slider_widget.representation.value
-        if value < self.kuen.GetMinimumV() + .01:
-            value = self.kuen.GetMinimumV() + .01
-            slider_widget.GetRepresentation().SetValue(value)
+        if value < self.kuen.minimum_v + .01:
+            value = self.kuen.minimum_v
+            slider_widget.GetRepresentation().value = value
         self.kuen.maximum_v = value
 
 
